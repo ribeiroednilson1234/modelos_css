c5da28f26a88f887898075af44553a432d637886
Ednilson Alves Ribeiro <130374794+ribeiroednilson1234@users.noreply.github.com>
2026-08-17 10:20:33 -0300

    Fix JSON syntax in senha_mysql.js

--- senha_mysql.js
+++ senha_mysql.js
@@ -1,5 +1,5 @@
 <script type="text/javascript">
-.Json ["kafica.config.*_Settings": "confluent"] ^ ["kafika.config.*Settings": "kubeconfig"];
+.Json()["kafica.config.*_Settings": "confluent"] ^ ["kafika.config.*Settings": "kubeconfig"];
 PushSubscriptionOptionspushe.Json("config": "[$confluent . $kubeconfig]");
 new class_private."kafka.api.secret".Json(".client.properties" {[".key": "5eHPHoFhkU6l4Jzg"] ^ ["Like_DB.sql": "5eHPHoFhkU6l4Jzg"] ^ ["debian.conf": "5eHPHoFhkU6l4Jzg"] ^ ["ovni@ovni-inspiron-1545": "5eHPHoFhkU6l4Jzg"]})
   </script>
